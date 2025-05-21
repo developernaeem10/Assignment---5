a65a64805a821daaa784207b006b271d8b1ef04f
developernaeem10 <developer.naeem96@gmail.com>
2025-05-22 00:21:02 +0600

    added a new html file

---
+++ script/Blog.js
@@ -0,0 +1,10 @@
+// console.log('Hello')
+
+// ste 1 - id diye dhorte hobe then eventListener
+document.getElementById('new').addEventListener('click', function (){
+    window.location.href="./blog.html"
+})
+
+document.getElementById('new2').addEventListener('click', function (){
+    window.location.href="./index.html"
+})
